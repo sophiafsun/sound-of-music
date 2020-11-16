701356d3614e51ca84fbd3f8d5806926fd797648
Amanda Stetz <amandastetz@amandas-mbp-2.myfiosgateway.com>
2020-11-15 21:10:14 -0500

    edited webpage

--- js/scroll.js
+++ js/scroll.js
@@ -8,7 +8,10 @@ $(window).scroll(function(){
         four = $('#four').offset().top,
         five = $('#five').offset().top,
         six = $('#six').offset().top,
-        seven = $('#seven').offset().top;
+        seven = $('#seven').offset().top,
+        eight = $('#eight').offset().top,
+        nine = $('#nine').offset().top,
+        ten = $('#ten').offset().top;
 
     var nav1 = $('a[href="#one"]'),
         nav2 = $('a[href="#two"]'),
@@ -16,48 +19,69 @@ $(window).scroll(function(){
         nav4 = $('a[href="#four"]'),
         nav5 = $('a[href="#five"]'),
         nav6 = $('a[href="#six"]'),
-        nav7 = $('a[href="#seven"]');
+        nav7 = $('a[href="#seven"]'),
+        nav8 = $('a[href="#eight"]'),
+        nav9 = $('a[href="#nine"]'),
+        nav10 = $('a[href="#ten"]');
 
     if(scrollPosition >= one) {
         $('.active').removeClass('active');
         $('#nav1').addClass('active');
-        nav1.contents('li').text('Intro');
+        nav1.contents('li').text('Title');
         nav1.siblings().contents('li').text('');
     }
     if(scrollPosition >= two) {
         $('.active').removeClass('active');
         $('#nav2').addClass('active');
-        nav2.contents('li').text('Vis 1');
+        nav2.contents('li').text('Text');
         nav2.siblings().contents('li').text('');
     }
     if(scrollPosition >= three) {
         $('.active').removeClass('active');
         $('#nav3').addClass('active');
-        nav3.contents('li').text('Vis 2');
+        nav3.contents('li').text('Vis 1');
         nav3.siblings().contents('li').text('');
     }
     if(scrollPosition >= four) {
         $('.active').removeClass('active');
         $('#nav4').addClass('active');
-        nav4.contents('li').text('More Text');
+        nav4.contents('li').text('Vis 2');
         nav4.siblings().contents('li').text('');
     }
     if(scrollPosition >= five) {
         $('.active').removeClass('active');
         $('#nav5').addClass('active');
-        nav5.contents('li').text('Vis 3');
+        nav5.contents('li').text('More text');
         nav5.siblings().contents('li').text('');
     }
     if(scrollPosition >= six) {
         $('.active').removeClass('active');
         $('#nav6').addClass('active');
-        nav6.contents('li').text('Vis 4');
+        nav6.contents('li').text('Vis 3');
         nav6.siblings().contents('li').text('');
     }
     if(scrollPosition >= seven) {
         $('.active').removeClass('active');
         $('#nav7').addClass('active');
-        nav7.contents('li').text('Conclusion');
+        nav7.contents('li').text('Vis 4');
         nav7.siblings().contents('li').text('');
+    }
+    if(scrollPosition >= eight) {
+        $('.active').removeClass('active');
+        $('#nav8').addClass('active');
+        nav8.contents('li').text('Audio Vis');
+        nav8.siblings().contents('li').text('');
+    }
+    if(scrollPosition >= nine) {
+        $('.active').removeClass('active');
+        $('#nav9').addClass('active');
+        nav9.contents('li').text('Conclusion');
+        nav9.siblings().contents('li').text('');
+    }
+    if(scrollPosition >= ten) {
+        $('.active').removeClass('active');
+        $('#nav10').addClass('active');
+        nav10.contents('li').text('About Us');
+        nav10.siblings().contents('li').text('');
     }
 });
